768b4ad7bb7a6fc39bf447da853a3b75f36cc54a
Bradley Showen <bshowen@live.seminolestate.edu>
2022-06-16 19:49:39 -0400

    Refactor this into an to ES6 class

--- src/dom/Ship.js
+++ src/dom/Ship.js
@@ -1,46 +1,65 @@
 import { HtmlElement } from "@bshowen/htmlelement";
 import "./Ship.css";
 /**
- * This is a draggable ship. This function creates the ships that the user can
+ * This is a draggable ship. This class creates the ships that the user can
  * drag onto their battle field.
  */
-export default function Ship(length) {
-  const ship = new HtmlElement({
-    type: "div",
-    id: `ship-${length}`,
-    attributes: {
-      draggable: "true",
-    },
-  });
+export default class Ship {
+  constructor(length) {
+    this.container = new HtmlElement({
+      type: "div",
+      id: `ship-${length}`,
+      attributes: {
+        draggable: "true",
+      },
+    });
 
-  ship.addEventListener("dragstart", dragStart);
+    // The length of this ship.
+    this.length = length;
 
-  for (let i = 0; i < length; i++) {
-    ship.appendChild(new shipBody(i, setDataSet));
+    this.container.addEventListener("dragstart", this.dragStart);
+
+    this.setDataSet = this.setDataSet.bind(this);
+
+    this.createShipBody();
   }
 
-  function setDataSet(int) {
-    ship.setAttribute("data-clicked-body", int);
-    ship.setAttribute("data-orientation", "vertical");
-    ship.setAttribute("data-length", length);
+  // Create the individual squares that makeup a ship.
+  createShipBody() {
+    for (let i = 0; i < this.length; i++) {
+      this.container.appendChild(this.shipBody(i));
+    }
   }
 
-  // A ship is composed of multiple square elements.
-  function shipBody(location, callBack) {
+  // This mousedown handler for this square.
+  setDataSet(int) {
+    this.container.setAttribute("data-clicked-body", int);
+    this.container.setAttribute("data-orientation", "vertical");
+    this.container.setAttribute("data-length", this.length);
+  }
+
+  /**
+   * Create the individual squares that makeup this ship.
+   * location = int
+   * The location is used to indicate which square the user picked up the ship
+   * from. If the ship is 3 squares, the user can pick the ship up from square 0,
+   * square 1, or square 2. This value is needed in order for other functions
+   * to generate coordinates to place the ship.
+   * */
+  shipBody(location) {
     const body = new HtmlElement({
       type: "div",
       classList: ["ship-body"],
     });
 
-    body.addEventListener("mousedown", callBack.bind(null, location));
+    body.addEventListener("mousedown", this.setDataSet.bind(null, location));
 
     return body;
   }
 
-  function dragStart(e) {
+  // Start the dragging process.
+  dragStart(e) {
     e.dataTransfer.setData("text/plain", e.target.id);
     e.dataTransfer.dropEffect = "move";
   }
-
-  return { container: ship };
 }
