5788129437b885e0bc5e2f9352892e4b8d16cfe8
Bradley Showen <bshowen@live.seminolestate.edu>
2022-07-20 17:48:33 -0400

    Add and test the method to determine a ships buffer coordinates

--- src/game_modules/Ship.js
+++ src/game_modules/Ship.js
@@ -36,4 +36,45 @@ export default class Ship {
   isSunk() {
     return this.#length === this.#hits;
   }
+
+  /**
+   * This method returns an instance of coordinateList.js, populated with the
+   * coordinates surrounding the ship. These are coordinates that other ships
+   * cannot be placed on. In this game, ships cannot touch
+   */
+  getBufferCoords() {
+    /**
+     * iterate through each shipCoord.
+     *   we are on currentCoord.
+     *   push the west coord into buffer coords if its not in ship
+     *   push the north coord into buffer coords if its not in ship
+     *   push the east coord into buffer coords if its not in ship
+     *   push the south coord into buffer coords if its not in ship
+     * next iteration
+     */
+
+    const bufferCoords = coordinateList();
+
+    this.#shipCoords.all().forEach((coordinate) => {
+      const [y, x] = coordinate; //y == int, x == int
+
+      if (!this.#shipCoords.contains([[y, x - 1]])) {
+        bufferCoords.add([[y, x - 1]]);
+      }
+
+      if (!this.#shipCoords.contains([[y, x + 1]])) {
+        bufferCoords.add([[y, x + 1]]);
+      }
+
+      if (!this.#shipCoords.contains([[y - 1, x]])) {
+        bufferCoords.add([[y - 1, x]]);
+      }
+
+      if (!this.#shipCoords.contains([[y + 1, x]])) {
+        bufferCoords.add([[y + 1, x]]);
+      }
+    });
+
+    return bufferCoords;
+  }
 }
--- src/game_modules/Ship.test.js
+++ src/game_modules/Ship.test.js
@@ -90,3 +90,64 @@ describe("Some methods are undefined without coordinates", () => {
     expect(ship.name).toBe("Carrier");
   });
 });
+
+describe("Ships return their buffer coordinates", () => {
+  let verticalShip;
+  let horizontalShip;
+  beforeEach(() => {
+    verticalShip = new Ship(
+      [
+        [3, 3],
+        [4, 3],
+        [5, 3],
+      ],
+      "carrier"
+    );
+    horizontalShip = new Ship(
+      [
+        [3, 3],
+        [3, 4],
+        [3, 5],
+      ],
+      "carrier"
+    );
+  });
+
+  test("A vertical ship returns the correct buffer coordinates", () => {
+    const expected = [
+      [2, 3],
+      [3, 4],
+      [4, 4],
+      [5, 4],
+      [6, 3],
+      [5, 2],
+      [4, 2],
+      [3, 2],
+    ];
+
+    const received = verticalShip.getBufferCoords();
+
+    expected.forEach((coordinate) => {
+      expect(received.contains([coordinate])).toBeTruthy();
+    });
+  });
+
+  test("A horizontal ship returns the correct buffer coordinates", () => {
+    const expected = [
+      [3, 2],
+      [2, 3],
+      [4, 3],
+      [2, 4],
+      [4, 4],
+      [2, 5],
+      [4, 5],
+      [3, 6],
+    ];
+
+    const received = horizontalShip.getBufferCoords();
+
+    expected.forEach((coordinate) => {
+      expect(received.contains([coordinate])).toBeTruthy();
+    });
+  });
+});
